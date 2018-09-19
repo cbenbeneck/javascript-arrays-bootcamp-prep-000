f60989e21a20dd357253febf791c84aa8a85a1f8
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-09-19 19:12:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) { 
   console.log(addElementToBeginningOfArray)
-  return array.unshift(array);
+  return array.unshift();
 }
 
 
