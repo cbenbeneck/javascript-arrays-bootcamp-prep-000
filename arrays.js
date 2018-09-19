78ea81fbf5c4e1a445a39395bbdeb1d6d6c61ade
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-09-19 19:45:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,8 +15,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
-function addElementToEndOfArray (array, element) {
-  return [array,...element]
+function addElementToEndOfArray(array, element) {
+  return [element,...array]
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
